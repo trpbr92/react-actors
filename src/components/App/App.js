d2ad90f459604ae3af3a2ba5e36fe192183b8642
Trace Brandt <trpbr92@gmail.com>
2021-03-24 13:54:31 -0500

    working on handleChange, text won't append

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,18 +6,24 @@ import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
 
 function App(){
-    const [join, setJoin] = useState('');
+    const [join, setJoin] = useState('Thanks for Joining!');
     
     const clickHandle = ()=>{
-      console.log('click');
-      console.log('join');
+      console.log('clicked Join Us!');
+      console.log(join);
     }
+    const handleChange = (event)=>{
+      console.log('in handleChange', event.target.value);
+      setText( );
+    }
+    const[text, setText] = useState('text');
     
     return (
       <div className="App">
         <Header />
         <About />
-        <input type="text" placeholder="First Name"/><input type="text" placeholder="Last Name"/><input type="text" placeholder="Best Impression"/><button onClick={clickHandle}>Join Us!</button>
+        <input type="text" placeholder="First Name"/> <input type="text" placeholder="Last Name"/><input type="text" placeholder="Best Impression" onChange={handleChange}></input> <button onClick={clickHandle}>Join Us!</button>
+       <p>{text}</p>
         <Contact />
         <Footer />
       </div>
