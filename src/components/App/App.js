946babfa5bcb51b04800889708960d4309aee3cc
Trace Brandt <trpbr92@gmail.com>
2021-03-24 10:32:37 -0500

    contact created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from '../Header/Header';
 import About from '../About/About';
+import Contact from '../Contact/Contact';
 
 function App() {
   return (
 <div>
   <Header />
   <About />
+  <Contact />
 </div>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact(){
+    return(
+        <div>
+            <h2>Contact</h2>
+            <p>Phone: 1-800-WE-REACT</p>
+            <p>Email: info@ReactActors.com</p>
+        </div>
+    )
+}
+export default Contact;
