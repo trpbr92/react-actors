ae4d925cfba1ceedf110d72cab6cf9c90a80cb15
Trace Brandt <trpbr92@gmail.com>
2021-03-24 10:29:46 -0500

    about created

---
+++ src/components/About/About.jsx
@@ -0,0 +1,9 @@
+function About(){
+    return(
+        <div>
+            <h2>About</h2>
+            <p> We are actors working with actors and we react to things.</p>
+        </div>
+    )
+}
+export default About;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <div>
+            <h1>React Actors</h1>
+        </div>
+    )
+}
+export default Header;
