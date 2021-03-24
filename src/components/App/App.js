97a6a147229f94c986b8d2f04845a19524bd2aff
Trace Brandt <trpbr92@gmail.com>
2021-03-24 13:28:48 -0500

    join us button functioning, not doing anything yet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import About from '../About/About';
 import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
 
-class App extends Component {
-  render() {
+function App(){
+    const [join, setJoin] = useState('');
+    
+    const clickHandle = ()=>{
+      console.log('click');
+      console.log('join');
+    }
+    
     return (
       <div className="App">
         <Header />
         <About />
+        <input type="text" placeholder="First Name"/><input type="text" placeholder="Last Name"/><input type="text" placeholder="Best Impression"/><button onClick={clickHandle}>Join Us!</button>
         <Contact />
         <Footer />
       </div>
     );
-  }
+  
 }
 
 export default App;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,6 @@ function Contact(){
     return(
         <div>
             <h2>Contact</h2>
-            <input type="text" placeholder="First Name"/><input type="text" placeholder="Last Name"/><input type="text" placeholder="Best Impression"/><button>Join Us!</button>
             <p>Phone: 1-800-WE-REACT</p>
             <p>Email: info@ReactActors.com</p>
         </div>
